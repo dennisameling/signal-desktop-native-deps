4cf66ebe9cd65a113fe9a723bcc7a536dc3f3b70
Dennis Ameling <dennis@dennisameling.com>
2021-05-07 11:19:37 +0200

    Try to fix appveyor config

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git $HOME/depot_tools
+export PATH=$HOME/depot_tools:$PATH
+if [[ $APPVEYOR_BUILD_WORKER_IMAGE == 'macOS' ]]; then
+    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+    rustup target add aarch64-apple-darwin
+    RINGRTC_PLATFORM=mac
+else
+    rustup target add aarch64-pc-windows-msvc
+    rustup target add i686-pc-windows-msvc
+    RINGRTC_PLATFORM=windows
+fi
+git clone --single-branch --branch add-windows-multi-arch-support dennisameling/ringrtc
