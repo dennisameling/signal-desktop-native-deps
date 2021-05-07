f0caa11eb8741296b68c455c695b2677dbef4fd5
Dennis Ameling <dennis@dennisameling.com>
2021-05-07 11:22:37 +0200

    Fix rust installation

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git $HOME/depot_tools
 export PATH=$HOME/depot_tools:$PATH
 if [[ $APPVEYOR_BUILD_WORKER_IMAGE == 'macOS' ]]; then
-    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
     rustup target add aarch64-apple-darwin
     RINGRTC_PLATFORM=mac
 else
